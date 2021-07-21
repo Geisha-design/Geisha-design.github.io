4c09e39ca42a3bf07d5814c6d12566f64cb97951
齐雨争 <48551712+Geisha-design@users.noreply.github.com>
2021-07-21 23:31:52 +0800

    Site updated: 2021-07-21 23:31:52

--- js/totop.js
+++ js/totop.js
@@ -1,12 +1,11 @@
 $(window).scroll(function() {
-    $(window).scrollTop() > $(window).height()*0.5 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
+    $(window).scrollTop() > 500 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
 });
-
 $("#rocket").click(function() {
     $("#rocket").addClass("launch");
     $("html, body").animate({
         scrollTop: 0
-    }, 1000, function() {
+    }, 500, function() {
         $("#rocket").removeClass("show launch");
     });
     return false;
